21a5150ac39a3c77f24ef793bb895643be9eba75
Rahul <rahulv0530@gmail.com>
2021-05-21 19:15:05 +0530

    Done Caching

--- src/js/feed.js
+++ src/js/feed.js
@@ -4,53 +4,104 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-
-    deferredPrompt.userChoice.then(choice => {
-      console.log(choice);
-    })
-
-    deferredPrompt = null;
-  }
+    createPostArea.style.display = 'block';
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(choice => console.log(choice))
+        deferredPrompt = null;
+    }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+    createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
-
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+// const onSaveButtonClicked = (event) => {
+//   console.log('Button clicked')
 
-function createCard() {
-  var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-  var cardTitle = document.createElement('div');
-  cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
-  cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
-  cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement('h2');
-  cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'San Francisco Trip';
-  cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement('div');
-  cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'In San Francisco';
-  cardSupportingText.style.textAlign = 'center';
-  cardWrapper.appendChild(cardSupportingText);
-  componentHandler.upgradeElement(cardWrapper);
-  sharedMomentsArea.appendChild(cardWrapper);
+//   // On-Demand Caching
+//   if ('caches' in window) {
+//     caches.open('On-Demand-Cache')
+//       .then((cache) => {
+//         cache.add('https://httpbin.org/get');
+//         cache.add('/src/images/sf-boat.jpg');
+//       });
+//   }
+// }
+
+const clearCards = () => {
+    while (sharedMomentsArea.hasChildNodes()) {
+        sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+    }
 }
 
-fetch('https://httpbin.org/get')
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    createCard();
-  });
+function createCard() {
+    var cardWrapper = document.createElement('div');
+    cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+    var cardTitle = document.createElement('div');
+    cardTitle.className = 'mdl-card__title';
+    cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+    cardTitle.style.backgroundSize = 'cover';
+    cardTitle.style.height = '180px';
+    cardWrapper.appendChild(cardTitle);
+    var cardTitleTextElement = document.createElement('h2');
+    cardTitleTextElement.className = 'mdl-card__title-text';
+    cardTitleTextElement.textContent = 'San Francisco Trip';
+    cardTitle.appendChild(cardTitleTextElement);
+    var cardSupportingText = document.createElement('div');
+    cardSupportingText.className = 'mdl-card__supporting-text';
+    cardSupportingText.textContent = 'In San Francisco';
+    cardSupportingText.style.textAlign = 'center';
+    // var cardSaveButton = document.createElement('button');
+    // cardSaveButton.textContent = 'Save';
+    // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+    // cardSupportingText.appendChild(cardSaveButton);
+    cardWrapper.appendChild(cardSupportingText);
+    componentHandler.upgradeElement(cardWrapper);
+    sharedMomentsArea.appendChild(cardWrapper);
+}
+
+/*
+  Cache, then Network Strategy
+*/
+
+const url = 'https://httpbin.org/get';
+let networkDataReturned = false;
+
+// From Network
+fetch(url)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        networkDataReturned = true;
+        console.log("From Network", data)
+
+        // Override with updated data
+        clearCards();
+        createCard();
+    });
+
+// From Cache
+if ('caches' in window) {
+    caches.match(url)
+        .then(function (response) {
+            if (response) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log('From Cache', data);
+
+            // Override if not already updated
+            if (data && !networkDataReturned) {
+                clearCards();
+                createCard();
+            }
+        });
+}
+
+
--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
+const STATIC_CACHE = 'Pre-Cache-v4'
+const DYNAMIC_CACHE = 'Dynamic-Cache-v1'
+const ON_DEMAND_CACHE = 'On-Demand-Cache'
+const STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 // Install Event
 self.addEventListener('install', (event) => {
@@ -9,24 +26,12 @@ self.addEventListener('install', (event) => {
 
     // This is same as await
     event.waitUntil(
-        caches.open('Pre-Cache')
+        caches.open(STATIC_CACHE)
             .then((cache) => {
                 console.log('[SW] Pre-Caching App Shell')
 
                 // Adding Files to Cache
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/main-image.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                ])
+                cache.addAll(STATIC_FILES)
             })
     )
 
@@ -35,8 +40,48 @@ self.addEventListener('install', (event) => {
 // Activate Event
 self.addEventListener('activate', (event) => {
     // console.log('[SW] Activating Service Worker: ', event)
+
+    // Cleaning Older Cache Store
+    event.waitUntil(
+        caches.keys()
+            .then((keyList) => {
+                return Promise.all(keyList.map(key => {
+                    if (key !== STATIC_CACHE && key !== DYNAMIC_CACHE && key !== ON_DEMAND_CACHE) {
+                        console.log('[SW] Removing Old Cache: ', key)
+                        return caches.delete(key)
+                    }
+                }))
+            })
+    )
+
     return self.clients.claim();
 })
+
+function isInArray(string, array) {
+    var cachePath;
+    if (string.indexOf(self.origin) === 0) {
+        console.log('matched ', string);
+        cachePath = string.substring(self.origin.length);
+    } else {
+        cachePath = string; // store the full request (for CDNs)
+    }
+    return array.indexOf(cachePath) > -1;
+}
+
+
+// Recursively Delete the Oldest Item from the Cache
+function trimCache(cacheName, maxItems) {
+    caches.open(cacheName)
+        .then((cache) => {
+            return cache.keys()
+                .then((keys) => {
+                    if (keys.length > maxItems) {
+                        cache.delete(keys[0])
+                            .then(trimCache(cacheName, maxItems));
+                    }
+                })
+        })
+}
 
 
 /*
@@ -47,17 +92,109 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     // console.log('[SW] Fetch Event: ', event)
 
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                } else {
-                    return fetch(event.request);
-                }
-            })
-    )
+    const url = 'https://httpbin.org/get';
+
+    if (event.request.url.indexOf(url) > -1) {
+
+        // Cache, then Network Strategy
+        event.respondWith(
+            caches.open(DYNAMIC_CACHE)
+                .then((cache) => {
+                    return fetch(event.request)
+                        .then((response) => {
+
+                            // Dynamically Caching the updated data
+                            cache.put(event.request.url, response.clone())
+                            return response;
+                        })
+                })
+        )
+
+    } else if (isInArray(event.request.url, STATIC_FILES)) {
+
+        // Cache-only Strategy with Static Files
+        event.respondWith(
+            caches.match(event.request)
+        )
+
+    } else {
+
+        // Cache with Network Fallback Strategy
+        event.respondWith(
+            caches.match(event.request)
+                .then((response) => {
+                    if (response) {
+                        return response;
+                    } else {
+
+                        // Dynamically Caching Items
+                        return fetch(event.request)
+                            .then((response) => {
+                                return caches.open(DYNAMIC_CACHE)
+                                    .then((cache) => {
+                                        console.log('[SW] Dynamically-Caching Items')
+
+                                        // Clone is needed as the response can be consumed only once
+                                        cache.put(event.request.url, response.clone())
+                                        return response;
+                                    })
+                            })
+                            .catch((error) => {
+                                console.log('[SW] Network Error: ', error)
+
+                                // Returning the Fallback Page conditionally
+                                if (event.request.headers.get('Accept').includes('text/html')) {
+                                    return caches.match('/offline.html');
+                                }
+                            })
+                    }
+                })
+        )
+    }
 })
+
+
+/*
+    Caching Strategies
+*/
+
+// Cache with Network Fallback
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request);
+//         }
+//       })
+//   );
+// });
+
+// Network with Cache Fallback
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request)
+//       .catch(function(err) {
+//         return caches.match(event.request);
+//       })
+//   );
+// });
+
+// Cache-only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
+
+// Network-only
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
 
 
 
